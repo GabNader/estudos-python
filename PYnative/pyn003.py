f8c67e4706dd8f5a3814a73563e960573244a5c8
Gabriel Nader Marini <gabriel.marini21@gmail.com>
2024-07-03 15:07:51 -0300

    exercício 3 e 4 pynative

--- PYnative/pyn003.py
+++ PYnative/pyn003.py
@@ -1,4 +1,6 @@
 # Write a program to accept a string from the user and display characters that are present at an even index number.
-texto = str(input('digite uma frase')).strip().capitalize()
-len(texto)
-# travei legal
+texto = str(input('digite uma frase'))
+print(f'Original String is {texto}')
+print('Printing only even index chars')
+for letra in range(0, len(texto), 2):
+    print(texto[letra])
---
+++ PYnative/pyn004.py
@@ -0,0 +1,7 @@
+#Write a program to remove characters from a string starting from zero up to n and return a new string.
+def remove_chars(string, num):
+    print(string[num:])
+
+
+remove_chars('pynative', 4)
+remove_chars('pynative', 2)
